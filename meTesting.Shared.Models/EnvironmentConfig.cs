3f8da410f0e9ac8f0f69aa635dbb6a3c8f7be8f9
Hadi Bazmi <exitxhu@gmail.com>
2024-10-22 12:43:19 +0330

    Add project files.

---
+++ meTesting.Shared.Models/EnvironmentConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace meTesting.Shared.Models;
+
+public class EnvironmentConfig
+{
+    public string AppKey { get; set; }
+    public string Url { get; set; }
+    public AppDetails Apps { get; set; }
+}
+
+public class AppDetails
+{
+    public string Automation { get; set; }
+    public string Bus { get; set; }
+    public string Aether { get; set; }
+    public string HRM { get; set; }
+}
